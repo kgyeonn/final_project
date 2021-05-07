af3a00fda4c7ff478beba23a4082c48d3e8156a4
kgyeonn <kgyeonn@gmail.com>
2021-05-07 10:44:21 +0900

    test2 이해한사람 확인용

---
+++ project3/src/project3/test2.java
@@ -0,0 +1,13 @@
+package project3;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이해하신사람 이름 작성 : ");
+		System.out.println("김가연");
+		
+
+	}
+
+}
