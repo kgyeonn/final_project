7812d425c33be3ecde7a9048fac6653852e935ec
smhrd <smhrd@211.227.114.165>
2021-05-07 10:47:40 +0900

    hi

--- project3/src/project3/test2.java
+++ project3/src/project3/test2.java
@@ -4,7 +4,7 @@ public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("이해하신사람 이름 작성 : ");
+		System.out.println("이해하신사람 이름 작성 : 전준엽 ");
 		System.out.println("김가연");
 		
 
