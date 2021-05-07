e79fbbffc633e25d6cc871f8942802022e963623
smhrd <smhrd@221.156.60.69>
2021-05-07 10:49:28 +0900

    돼요?

--- project3/src/project3/test2.java
+++ project3/src/project3/test2.java
@@ -5,6 +5,7 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("이해하신사람 이름 작성 : 전준엽 ");
+		System.out.println("이해하신사람 이름 작성 : 정용현 ");
 		System.out.println("김가연");
 		
 
