f5265f5f4738d4c55b3939b9f5eb0acd936bb52a
kgyeonn <kgyeonn@gmail.com>
2021-05-22 15:57:29 +0900

    커밋

--- project3/src/project3/MemberVO.java
+++ project3/src/project3/MemberVO.java
@@ -1,4 +1,4 @@
-package com;
+package project3;
 
 public class MemberVO {
    
---
+++ project3/src/project3/RankDAO.java
@@ -0,0 +1,67 @@
+package project3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RankDAO {
+	
+	private Connection conn;
+	   private PreparedStatement pst;
+	   private ResultSet rs;
+	   private int cnt,cnt1;
+	   
+	   
+	   public void conn() {
+		      try {
+		         Class.forName("oracle.jdbc.driver.OracleDriver");
+		         String db_url = "jdbc:oracle:thin:@localhost:1521:xe";
+		         String db_id = "hr";
+		         String db_pw = "hr";
+
+		         conn = DriverManager.getConnection(db_url, db_id, db_pw);
+		         
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		      }   
+		   }
+		   
+		   public void close() {
+		      try {
+		         if(rs != null) {
+		            rs.close();
+		         }   
+		         if (pst != null) {
+		            pst.close();
+		         }
+		         if(conn != null) {
+		            conn.close();
+		         }
+		      } catch (SQLException e) {
+		         e.printStackTrace();
+		      }
+		   }
+		   
+		   public void rankpoint(MemberVO vo) {
+			   conn();
+			   try {
+				   String sql = "SELECT SUM(MEMBER_POINT), COUNT(MEMBER_ID) FROM MEMBER WHERE REGION='±¤»ê±¸'";
+				   
+				   pst = conn.prepareStatement(sql);
+				   rs = pst.executeQuery();
+				   
+				   if(rs.next()) {
+					   int get_totalPoint = rs.getInt(1);
+					   int get_countId = rs.getInt(2);
+					   
+					   vo = new RankVO(get_totalPoint,get_countId);
+							   }
+			   
+				   
+			   }
+		   }
+	
+
+}
